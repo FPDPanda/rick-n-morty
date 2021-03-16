a168ec44905c24bd02a2147d6da03b19ff612ed5
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2021-03-16 19:27:53 -0300

    [Component] Creating - Main

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// CSS
+import '../styles/Main.css'
+
+function Main() {
+    return (
+        <main className="App-main">
+        </main>
+    )
+}
+
+export default Main
