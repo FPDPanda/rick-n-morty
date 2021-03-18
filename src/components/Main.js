0b4ad28a241f72e0f1d816a4167b5a7e4f2abefb
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2021-03-18 20:12:37 -0300

    [Component] Updating - Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,15 @@ import React from 'react'
 // CSS
 import '../styles/Main.css'
 
+// Components
+import Screen from './Screen'
+import Buttons from './Buttons'
+
 function Main() {
     return (
         <main className="App-main">
+            <Screen />
+            <Buttons />
         </main>
     )
 }
