48d7125ea99ebb507e336125211e84508e635ec2
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2021-03-18 20:11:21 -0300

    [Component] Creating - Buttons

---
+++ src/components/Buttons.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import {graphql} from 'react-apollo';
+
+// CSS
+import '../styles/Buttons.css'
+
+// Queries
+import {queryCharactersAndLocations} from '../queries/queries'
+
+function Buttons({data}) {
+    function createEpisode() {
+        let newEpisodeNumber = localStorage.length+1;
+        let newEpisodeCharacters = data.charactersByIds;
+        let newEpisodeLocations = data.locationsByIds;
+        let newEpisode = {
+            episodeNumber: newEpisodeNumber,
+            characters: newEpisodeCharacters,
+            locations: newEpisodeLocations,
+        }
+
+        let episodes = {
+            ...newEpisode
+        }
+
+        localStorage.setItem('episode' +newEpisodeNumber, JSON.stringify(episodes));
+        window.location.reload()
+    }
+    return (
+        <div className='App-main-buttons'>
+            <button 
+            className="App-main-buttons-create"
+            onClick={createEpisode}>
+                New episode
+            </button>
+        </div>
+    )
+}
+
+export default graphql(queryCharactersAndLocations)(Buttons);
